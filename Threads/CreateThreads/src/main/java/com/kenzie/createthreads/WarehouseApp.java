c41ffd62b3cf7ae02c883d90074f358892b7fc60
NicholeD <YOUREMAIL@YOUREMAIL.com>
2022-08-12 17:15:13 -0600

    Implemented Runnable in DeliveryManager class, DeliveryManager::run() and WarehouseApp::startDeliveryThread(). Tests pass.

--- Threads/CreateThreads/src/main/java/com/kenzie/createthreads/WarehouseApp.java
+++ Threads/CreateThreads/src/main/java/com/kenzie/createthreads/WarehouseApp.java
@@ -12,10 +12,7 @@ public class WarehouseApp {
      */
     public static void main(String [] args) {
         System.out.println("WarehouseApp thread started.");
-
-        deliveryWarehouse1 = new DeliveryManager(DeliveryTruck.deliverPackages());
-        deliveryWarehouse1.sortShipment();
-        deliveryWarehouse1.printInventory();
+        startDeliveryThread(DeliveryTruck.deliverPackages());
     }
 
     /**
@@ -23,7 +20,9 @@ public class WarehouseApp {
      * @param packages Delivered packages.
      */
     public static void startDeliveryThread(List<WarehousePackage> packages) {
-
+        deliveryWarehouse1 = new DeliveryManager(packages);
+        Thread deliveryWarehouseThread = new Thread(deliveryWarehouse1);
+        deliveryWarehouseThread.start();
     }
 
     public DeliveryManager getDeliveryWarehouse1() {
